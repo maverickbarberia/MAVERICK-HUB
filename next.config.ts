19e5958f86a51a06c33fa80fa1d09082dab34d5d
Juan David Figueroa <juandavidfigueroa4@gmail.com>
2026-09-08 09:12:03 -0500

    fix: error typescript escaner y next config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['192.168.128.19', '192.168.128.19:3000']
 };
 
 export default nextConfig;
--- src/app/admin/scanner/page.tsx
+++ src/app/admin/scanner/page.tsx
@@ -32,7 +32,6 @@ export default function AdminPage() {
                     }
                   }}
                   components={{
-                    audio: false,
                     finder: true,
                   }}
                 />
